0f274813f48e852f43f2f830425a75694f99273a
Pragnath Chintalapati <prag2002@uw.edu>
2021-01-15 20:50:42 -0800

    initial ProductCollectionView set up

---
+++ EcoStore/Objects/Cells.swift
@@ -0,0 +1,15 @@
+//
+//  Cells.swift
+//  EcoStore
+//
+//  Created by Pragnath Chintalapati on 1/15/21.
+//
+
+import Foundation
+import UIKit
+
+// Each ProductCell displays a single product.
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+}
