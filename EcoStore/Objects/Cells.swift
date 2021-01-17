751d85152e358c8c72caca680aa937e43f907a89
anshaysaboo <anshaysaboo1586@gmail.com>
2021-01-16 19:11:33 -0800

    Added profile screen

--- EcoStore/Objects/Cells.swift
+++ EcoStore/Objects/Cells.swift
@@ -38,3 +38,13 @@ class ShopHeaderCollectionReusableView: UICollectionReusableView {
     @IBOutlet weak var totalRaisedLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
 }
+
+class CardView: UIView {
+    override func layoutSubviews() {
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.1
+        self.layer.shadowOffset = CGSize(width: 0, height: 3)
+        self.layer.shadowRadius = 5
+        self.layer.cornerRadius = 10.0
+    }
+}
